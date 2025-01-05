6dad6fbf3112bbb66da6e8df813f9ab22a03d1e3
Seyed Amirhossein <amir.hossein.1384.musavi@gmail.com>
2025-01-05 20:43:49 +0330

    Update Map Pattern.py

--- Map Pattern.py
+++ Map Pattern.py
@@ -1,13 +1,35 @@
-         |   |   |   |   |   |   |   |   |   |
-    __ __|_ _|_ _|_ _|_ _|_O_|_ _|_ _|_ _|_ _|__ __                        \
-    __ __|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|__ __                        \ : Vertial wall
-    __ __|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|__ __                      ___ : Horizontal wall
-    __ __|_ _|_ _|_ _|_ _|_ _\_ _|_ _|_ _|_ _|__ __                        O : White piece
-    __ __|_ _|_ _|___|___|_ _\_ _|_ _|_ _|_ _|__ __                        0 : Black piece
-    __ __|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|__ __
-    __ __|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|__ __
-    __ __|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|__ __
-    __ __|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|_ _|__ __
-    __ __|_ _|_ _|_ _|_ _|_0_|_ _|_ _|_ _|_ _|__ __
-         |   |   |   |   |   |   |   |   |   |
-         |   |   |   |   |   |   |   |   |   |
+def draw_quoridor_board():
+    horizontal_wall = '\u2501'  
+    vertical_wall = '\u2503'   
+    empty_space = '   '   
+    wall = "\u2551"     
+    intersection = '\u254b'   
+
+    board = []
+
+    for row in range(17):
+        if row % 2 == 0:
+            line = []
+            for col in range(17):
+                if col % 2 == 0:
+                    line.append('   ')
+                else:
+                    line.append(vertical_wall)
+            board.append(''.join(line))
+        else:
+            line = []
+            for col in range(17):
+                if col % 2 == 0:
+                    line.append(horizontal_wall * 3)
+                else:
+                    line.append(intersection) 
+            board.append(''.join(line))
+
+    for line in board:
+        print(line)
+
+draw_quoridor_board()
+horizental = "\u2551"
+vertical = "\u2550"
+print(vertical)
+print(horizental)
