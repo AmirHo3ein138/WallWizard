319894370cb8ed00fc7f271668dccb568ed88e13
Seyed Amirhossein <amir.hossein.1384.musavi@gmail.com>
2025-01-05 21:00:44 +0330

    Update Map Pattern.py

--- Map Pattern.py
+++ Map Pattern.py
@@ -1,10 +1,8 @@
 def draw_quoridor_board():
-    horizontal_wall = '\u2501'  
-    vertical_wall = '\u2503'   
-    empty_space = '   '   
-    wall = "\u2551"     
-    intersection = '\u254b'   
-
+    horizontal_wall = '\u2508' 
+    vertical_wall = '\u2506'    
+    empty_space = '   '         
+    intersection = '\u254b'    
     board = []
 
     for row in range(17):
@@ -16,20 +14,16 @@ def draw_quoridor_board():
                 else:
                     line.append(vertical_wall)
             board.append(''.join(line))
-        else:
+        else: 
             line = []
             for col in range(17):
                 if col % 2 == 0:
                     line.append(horizontal_wall * 3)
                 else:
-                    line.append(intersection) 
+                    line.append(intersection)
             board.append(''.join(line))
 
     for line in board:
         print(line)
 
 draw_quoridor_board()
-horizental = "\u2551"
-vertical = "\u2550"
-print(vertical)
-print(horizental)
